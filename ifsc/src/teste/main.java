1df687c4cd3109281a81f8ccd0862b94e68340a0
ash <ash.m10@aluno.ifsc.edu.br>
2022-10-03 15:54:30 -0300

    Updates

--- ifsc/src/teste/main.java
+++ ifsc/src/teste/main.java
@@ -1,10 +1,19 @@
 package teste;
 
+import java.util.Scanner;
+
 public class main {
 
 	public static void main(String[] args) {
 		
+		Scanner leitura = new Scanner (System.in);
+		
 		System.out.println("hello world");
+		
+		System.out.println("tell me your name: ");
+		String nome = leitura.nextLine();
+		
+		System.out.println("your name is: "+nome);
 	}
 
 }
